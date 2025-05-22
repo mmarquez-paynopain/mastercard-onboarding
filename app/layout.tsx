2358216639bb7f09ef4ce8d0c3b4a55bc17df0f9
Miguel <miguel.marquez@paynopain.com>
2025-05-22 15:31:36 +0200

    use prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           type="module"
           src="https://src.mastercard.com/srci/integration/components/src-ui-kit/src-ui-kit.esm.js"
         ></script>
-        <script src="https://sandbox.src.mastercard.com/srci/integration/2/lib.js?srcDpaId=1cc38e29-2bf8-4318-8a98-8aad0a51c840_dpa0&locale=es_ES"></script>
+        <script src="https://src.mastercard.com/srci/integration/2/lib.js?srcDpaId=f8b5c36f-efd6-409a-8102-588dc785d707&locale=es_ES"></script>
       </head>
       <body>{children}</body>
     </html>
